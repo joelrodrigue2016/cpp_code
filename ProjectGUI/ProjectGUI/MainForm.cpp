1a5d06b24620f3f785bdc3a039601c6020dd1056
Joel Rodriguez <62555069+joelrodrigue2016@users.noreply.github.com>
2022-06-04 23:42:14 -0400

    Add files via upload
    Simple GUI that asks for name  and last name and greets the user back.

---
+++ ProjectGUI/ProjectGUI/MainForm.cpp
@@ -0,0 +1,14 @@
+#include "MainForm.h"
+
+
+
+using namespace System;
+using namespace System::Windows::Forms;
+void main(array<String^>^ args)
+{
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+	ProjectGUI::MainForm form;
+	Application::Run(% form);
+}
+
